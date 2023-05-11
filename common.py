cd8e6901879db1ce54529b0739dd25f9d8137734
melalaoui <melalaoui@thor.enseirb-matmeca.fr>
2023-05-11 08:55:09 +0200

    mock RSA key generation

--- common.py
+++ common.py
@@ -97,19 +97,38 @@ def expo_modulaire_fast(e,b,n):
 # retourne la liste des nombres premiers <= n
 # methode du crible d Eratosthene
 def crible_eras(n):
-    return []
+    primes = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n):
+        if (primes[p] == True):
+            for i in range(p * p, n+1, p):
+                primes[i] = False
+        p += 1
+    primes_only = []
+    for p in range(2, n):
+        if primes[p]:
+            primes_only.append(p)
+    return primes_only
+
         
 ####################
 # Q6
 ####################
-
 # input: n  
 # input: t number of tests
 # test if prime according to fermat
 # output: bool if prime 
-def test_fermat(n,t):
-    # random number generator between a and b
-    #x = random.randint(a,b)
+def test_fermat(n, t):
+    if n <= 1 or n == 4:
+        return False
+    if n <= 3:
+        return True
+
+    while t > 0:
+        a = random.randint(2, n - 2)
+        if expo_modulaire_fast(n-1,a, n) != 1:
+            return False
+        t -= 1
     return True
 
 ####################
@@ -121,7 +140,12 @@ def test_fermat(n,t):
 # for rabin test 
 # returns r,u such that 2^r * u = n and u is odd
 def find_ru(n):
-    return 0  
+    r = 0
+    u = n
+    while u % 2 == 0:
+        u //= 2
+        r += 1
+    return r, u
 
 ####################
 # Q8
@@ -131,16 +155,31 @@ def find_ru(n):
 #a entier dans [1,n-2]
 #pgcd(a,n)=1
 #retourne True , si a est un temoin de Rabin de non-primalite de n
-def temoin_rabin(a,n):
-    # utilisez expo_modulaire_fast !
-    return 0
+def temoin_rabin(a, n):
+    r, u = find_ru(n-1)
+    x = expo_modulaire_fast(u, a, n)
+    if x == 1 or x == n - 1:
+        return False
+    for _ in range(r - 1):
+        x = expo_modulaire_fast(2, x, n)
+        if x == n - 1:
+            return False
+    return True
 
 
 #n entier a tester, t nombre de tests
 #retourne True , si n est premier
 #retourne False , avec proba > 1-(1/4)**t, si n est compose
-def test_rabin(n,t):
-    return 0  
+def test_rabin(n, t):
+    if n == 2 or n == 3:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+    for _ in range(t):
+        a = random.randrange(2, n - 1)
+        if temoin_rabin(a, n):
+            return False
+    return True
             
 # prime generator
 # output: n range for prime number
@@ -148,7 +187,10 @@ def test_rabin(n,t):
 # pour generer un nombre premier sur n bits.
 # range de n: p = random.randint(pow(2,n-1),pow(2,n)-1)
 def gen_prime(n):
-    return 0
+    while True:
+        p = random.randint(pow(2, n - 1), pow(2, n) - 1)
+        if test_rabin(p, 5):  # 5 est un choix arbitraire pour le nombre de tests
+            return p
 
 ####################
 # Helper functions for rsa/elgamal
